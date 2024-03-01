b29c9f7f29a5bc2ef7304ae350b9245e4fcb470d
Chen Shang <cshang@amazon.com>
2024-02-29 16:56:17 -0800

    fix(TM-DS):  ds work! -02: add react-query to fetch mixers in KG

--- packages/scene-composer/src/components/SceneLayers3.tsx
+++ packages/scene-composer/src/components/SceneLayers3.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useLayoutEffect} from 'react';
+import React, { useContext, useEffect} from 'react';
 
 import { useQuery } from '@tanstack/react-query';
 
@@ -6,7 +6,6 @@ import { sceneComposerIdContext } from '../common/sceneComposerIdContext';
 import { useStore } from '../store';
 
 import { processQueries3 } from '../utils/entityModelUtils/processQueries3';
-
 import { getGlobalSettings } from '../common/GlobalSettings';
 
 export const SceneLayers3: React.FC = () => {
@@ -18,26 +17,29 @@ export const SceneLayers3: React.FC = () => {
 
     const renderSceneNodesFromLayers = useStore(sceneComposerId)((state) => state.renderSceneNodesFromLayers);
 
-    useLayoutEffect(() => {
-        const fetchData = async () => { 
-            try {
-                const nodes = await processQueries3(
-                    [`select entity, r, e
-                                from EntityGraph
-                                match (entity)-[r]->(e)
-                                where r.relationshipName = 'isChildOf'
-                                and e.entityId = 'Mixers_sceneid'`])
+    const nodes = useQuery({
+        queryKey: ['ds-test', sceneComposerId],
+        queryFn: async () => {
+          const nodes = await processQueries3(
+            [`select entity, r, e
+                        from EntityGraph
+                        match (entity)-[r]->(e)
+                        where r.relationshipName = 'isChildOf'
+                        and e.entityId = 'Mixers_sceneid'`])
+          return nodes;
+        },
+        refetchInterval: (_, query) => {
+          return !query.state.error && isViewing ? 30 : 0;
+        },
+        refetchOnWindowFocus: false,
+      });
 
-                renderSceneNodesFromLayers(nodes)
+    useEffect(() => {
 
-                console.log('ProcessQuery completed with result: ', nodes);
-            } catch(error) {
-                console.error('ProcessQueries reached error: ', error);
-            }
+        if (nodes.data) {
+            renderSceneNodesFromLayers(nodes.data)
         }
-
-        fetchData();
-    }, [sceneMetadataModule]);
+    }, [sceneMetadataModule, renderSceneNodesFromLayers, nodes.data]);
     
     return <>
     { console.log('ScenLayers3 return')}
